57e4b30711ab383311a820d1cb6aa1db2b3fc240
Camila B <Lcamila.bustos@yahoo.com>
2021-01-16 14:35:09 -0600

    Created one paddle

--- UbiGame_Blank/Source/Game/GameBoard.cpp
+++ UbiGame_Blank/Source/Game/GameBoard.cpp
@@ -6,8 +6,9 @@
 using namespace Game;
 
 GameBoard::GameBoard()
+	:m_player(nullptr)
 {
-
+	CreatePlayer();
 }
 
 
@@ -20,4 +21,17 @@ GameBoard::~GameBoard()
 void GameBoard::Update()
 {	
 	
+}
+
+void Game::GameBoard::CreatePlayer()
+{
+	m_player = new GameEngine::Entity();
+	GameEngine::GameEngineMain::GetInstance()->AddEntity(m_player);
+
+	m_player->SetPos(sf::Vector2f(200.f, 200.f));
+	m_player->SetSize(sf::Vector2f(10.f, 200.f));
+
+	GameEngine::RenderComponent* render = static_cast<GameEngine::RenderComponent*>(m_player->AddComponent<GameEngine::RenderComponent>());
+
+	render->SetFillColor(sf::Color::White);
 }
